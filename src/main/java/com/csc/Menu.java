3a6c9ed03d9d625b997fc28494d928a46dc6ccdf
Michael <michaelvargo@outlook.com>
2024-08-29 15:34:26 -0500

    Added my method

--- src/main/java/com/csc/Menu.java
+++ src/main/java/com/csc/Menu.java
@@ -1,3 +1,26 @@
 package com.csc;
 
-// Your code goes here!
+import java.util.Scanner;
+
+public class Menu {
+    Scanner input = new Scanner(System.in);
+
+    public String number() {
+        int number;
+        // user input
+        System.out.print("Enter your number:");
+        number = input.nextInt();
+        // input loop for invalid numbers
+        while (number < 1) {
+            System.out.println("number too small, enter a number:");
+            number = input.nextInt();
+        }
+        return String.format("Your number is %d.", number);
+    }
+
+    // driver
+    public static void main(String[] args) {
+        Menu james = new Menu();
+        System.out.print(james.number());
+    }
+}
